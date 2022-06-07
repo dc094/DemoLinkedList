f43725099cf31150c09f96c59770ee0556f4095a
Durgesh094 <durgeshchavan1994@gmail.com>
2022-06-07 21:43:10 +0530

    First Commit

---
+++ src/com/bl/demolinkedlist/LinkedListExample.java
@@ -0,0 +1,34 @@
+package com.bl.demolinkedlist;
+
+import java.util.*;
+public class LinkedListExample {
+    public static void main(String args[])
+    {
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        linkedList.add("Swapnil");
+        linkedList.add("Surendra");
+        linkedList.add(1, "is a friend of");
+
+        linkedList.remove(2);
+        linkedList.add(2, "Bhushan");
+
+        linkedList.removeLast();
+
+        linkedList.addLast("Jayesh");
+
+        // Using the Get method and the
+        // for loop
+        for (int i = 0; i < linkedList.size(); i++) {
+
+            System.out.print(linkedList.get(i) + " ");
+        }
+
+        System.out.println();
+
+        // Using the for each loop
+        for (String str : linkedList)
+            System.out.print(str + " ");
+    }
+}
+
